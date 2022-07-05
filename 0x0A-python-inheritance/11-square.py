d7182047c10b56c82f89c538aa85fda58d0bf1a1
Amara Nechey <37805319+NecheyGrace@users.noreply.github.com>
2022-07-05 02:37:29 +0100

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -34,3 +34,20 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         """informal string representation of the rectangle"""
         return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """A representation of a square"""
+    def __init__(self, size):
+        """instantiation of the square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """"returns the area of the square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """informal string reepresentation of the square"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
