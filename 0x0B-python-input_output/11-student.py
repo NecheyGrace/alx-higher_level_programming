55ce8988f11c09ce748220bf681af0e95c85b628
Amara Nechey <37805319+NecheyGrace@users.noreply.github.com>
2022-07-06 02:00:24 +0100

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-""" Module that defines the class Student
+"""initializate
 """
 
 
 class Student:
-    """ Class to create student instances """
+    """Class Student
+    """
 
     def __init__(self, first_name, last_name, age):
-        """ Special method to initialize """
+        """initialize method
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """ Method that returns directory description """
-        return self.__dict__.copy()
+    def to_json(self, attrs=None):
+        """to_json
+        """
+        dic = {}
+        if attrs is None:
+            return self.__dict__
+        for i in attrs:
+            if hasattr(self, i):
+                dic[i] = getattr(self, i)
+        return dic
+
+    def reload_from_json(self, json):
+        for k, v in json.items():
+            if hasattr(self, k):
+                setattr(self, k, v)
