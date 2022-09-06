1742fb16ab9d7fbdbe67ce01f8aae56114001475
Amara Nechey <37805319+NecheyGrace@users.noreply.github.com>
2022-09-06 23:07:38 +0100

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
-
-let numbers = process.argv.slice(2);
-numbers = numbers.map(number => parseInt(number, 10));
-
-let biggest = 0;
-let secondBiggest = 0;
-
-for (const num of numbers) {
-  if (num >= biggest) {
-    secondBiggest = biggest;
-    biggest = num;
-  }
-  if (num < biggest && num >= secondBiggest) secondBiggest = num;
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
-
-console.log(secondBiggest);
