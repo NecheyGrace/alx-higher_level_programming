d7196e69b5338700f0498fa74659523d250ea4bc
Amara Nechey <37805319+NecheyGrace@users.noreply.github.com>
2022-09-06 23:03:27 +0100

    Update 102-add_me_maybe.js

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-// JS Script
-
-exports.addMeMaybe = function (number, theFunction) {
-  theFunction(number + 1);
-};
+const fs = require('fs');
+const src1 = fs.readFileSync(process.argv[2], 'utf8');
+const src2 = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], src1 + src2);
