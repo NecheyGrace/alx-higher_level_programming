20e2c9e9ec573a546fac715e607ff060228de1a5
Amara Nechey <37805319+NecheyGrace@users.noreply.github.com>
2022-09-06 21:08:34 +0100

    Update 7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const number = parseInt(argv[2]);
+for (let i = 0; i < number; i++) {
+  console.log('C is fun');
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
