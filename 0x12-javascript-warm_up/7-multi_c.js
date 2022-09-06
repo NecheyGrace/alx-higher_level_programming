a2e069127bd98bb8357ecb4e8ae9491660ffb859
Amara Nechey <37805319+NecheyGrace@users.noreply.github.com>
2022-09-06 21:23:42 +0100

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
-let i = parseInt(process.argv[2]);
-if (Number.isNaN(i)) {
-  console.log('Missing number of occurrences');
+const { argv } = require('process');
+const number = parseInt(argv[2]);
+if (!number) {
+  console.log('Missing size');
 } else {
-  while (i > 0) {
-    console.log('C is fun');
-    i--;
+  for (let i = 0; i < number; i++) {
+    let concat = '';
+    for (let j = 0; j < number; j++) {
+      concat += 'X';
+    }
+    console.log(concat);
   }
 }
