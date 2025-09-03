890d0abe0b590d0788f5f7079f3a2c4ff4ff5d9b
Amara Nechey <37805319+NecheyGrace@users.noreply.github.com>
2025-09-03 11:30:09 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # Script that takes a URL, sends a request using curl, and displays the response body size in bytes
-curl -s -w "%{size_download}" -o /dev/null "$1" | echo "$(cat) bytes"
-
-
-
-
+curl -s -w "%{size_download}" -o /dev/null "$1"
